8aef2897a00febdc393986eaba5d7665572ec73f
victorjurj <victor.jurj@yahoo.com>
2023-10-05 12:03:53 +0300

    extract BookService

--- src/main/java/com/fortech/academy/library/controller/BooksController.java
+++ src/main/java/com/fortech/academy/library/controller/BooksController.java
@@ -1,6 +1,7 @@
 package com.fortech.academy.library.controller;
 
 import com.fortech.academy.library.entities.Book;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,13 @@ import java.util.List;
 @RequestMapping("books")
 public class BooksController {
 
+    private final BooksService booksService;
+
+    @Autowired
+    public BooksController(BooksService booksService) {
+        this.booksService = booksService;
+    }
+
     @GetMapping("test")
     public String test() {
         return "It works...";
@@ -19,11 +27,7 @@ public class BooksController {
 
     @GetMapping
     public List<Book> getAllBooks() {
-        List<Book> responseBody = new ArrayList<>();
-        responseBody.add(new Book(true, "978-025489", "Robert C. Martin", "Clean Code", 2008));
-        responseBody.add(new Book(true, "978-059988", "Robert C. Martin", "Clean Architecture", 2017));
-        responseBody.add(new Book(true, "978-116688", "Martin Fowler", "Refactoring: Improving the Design of Existing Code (2nd Edition)", 2018));
-        return responseBody;
+        return booksService.getAllBooks();
     }
 
 }
